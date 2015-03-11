310b28919302b13857167577ada8f6084440e7c3
Rohith Yeluri <rohithyeluri@users.noreply.github.com>
2015-03-12 10:57:54 +1300

    Added router

---
+++ routes.js
@@ -0,0 +1,13 @@
+$(bindKeys);
+
+function bindKeys(){};
+$('body').on('keyup', function(e){
+
+});
+/*function getRandomLocation(){
+  var height = $('#container').height();
+  var width = $('#container').width();
+  var left = Math.floor(Math.random() * (width - 1) + 1);
+  var top = Math.floor(Math.random() * (height - 1) + 1);
+  return {left: left, top: top}
+}*/
