856bf6d4730d51b24267c8a42b715766f3c807c6
philldellow <philldellow@gmail.com>
2015-03-12 16:33:17 +1300

    on advice, dropped the loop for brackets and moving to a key press and stop function

--- model/CurlyBrackets.js
+++ model/CurlyBrackets.js
@@ -1,6 +1,13 @@
 var insertAudioTag = function(){
 	$('body').append$("<audio></audio>");
-}
+};
+
 var insertMP3 = function(){
-	$('audio').append$("/MP3")
-}
+	$('audio').append$("/MP3s/CurleyBracketsLoop.wav");
+};
+
+var play = $('audio').attr('autoplay','true');
+
+var repeat  = true;
+
+$(document).ready(insertAudioTag.insertMP3.play.repeat);
