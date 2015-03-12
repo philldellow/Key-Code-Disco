4adb0f828741d5b8f99103559292abd565a03257
Rohith Yeluri <rohithyeluri@users.noreply.github.com>
2015-03-13 11:05:03 +1300

    Changed else ifs to if

--- router.js
+++ router.js
@@ -9,45 +9,45 @@ $(document).on('keypress', function(e){
 	    test.delayFlash();
 	}
 
-else if (e.keyCode === 59){
+  if (e.keyCode === 59){
 		v = new semiColon();
 		v.semi();
 	}
 
-  else if (e.keyCode === 123){
+  if (e.keyCode === 123){
 		c = new curlyBracketStart();
 		c.curlystartLoop();
 	}
 
-  else if (e.keyCode === 125){
+  if (e.keyCode === 125){
 		d = new curlyBracketend();
 		d.curlystoploop();
 	}
 
-  else if(e.keyCode === 13){
+  if(e.keyCode === 13){
 		e = new enter();
 		e.ent();
 	}
 
-  else if (e.keyCode === 40){
+  if (e.keyCode === 40){
 		f = new roundBracketStart();
 		f.roundstartLoop();
 	}
 
-  else if (e.keyCode === 41){
+  if (e.keyCode === 41){
 		f = new roundBracketEnd();
 		f.endloopround();
 	}
-  else if (e.keyCode === 91){
+  if (e.keyCode === 91){
 		f = new squareBracketStart();
 		f.squarestartLoop();
 	}
 
-  else if (e.keyCode === 93){
+   if (e.keyCode === 93){
 		f = new squareBracketEnd();
 		f.endloopsquare();
 	}
-  else if(e.keyCode === 32){
+  if(e.keyCode === 32){
     var spaceBarController = new SpaceBarController();
     spaceBarController.Display();
   }
